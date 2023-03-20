67e561258cb5392dfbc344345495e06eb76c19b3
Jarvx <jarvx404@gmail.com>
2023-03-20 19:39:02 +0000

    fixes

--- src/components/MainHero.jsx
+++ src/components/MainHero.jsx
@@ -1,7 +1,10 @@
+import ltw from "../assets/logo-t-white.png"
+
 import { useState, useEffect } from 'react';
 import TypeWriterEffect from 'react-typewriter-effect';
 import React from 'react'
 import { useNavigate } from "react-router-dom";
+
 //How can I put these in an array as a one-line if not *.jpg :(
 import heroImage1 from '../assets/nationala/harambe-flag.jpg'
 import heroImage2 from '../assets/nationala/harambe-teaborgs.jpg'
@@ -33,7 +36,8 @@ function MainHero(){
         <div className="hero-overlay bg-opacity-70"></div>
 
         <div className="hero-content text-center text-neutral-content">
-                <div className="max-w-7xl">
+                <div className="max-w-7xl flex flex-col align-middle justify-center items-center">
+                    <img src={ltw} className="w-[10rem] mb-[2rem]" alt="" />
                     <h1 className="mb-7 lg:text-8xl text-6xl font-bold text-white">
                         Harambe Cartel
                     </h1>
@@ -43,16 +47,12 @@ function MainHero(){
        
                 <TypeWriterEffect
                         textStyle={{
-                            fontSize: '1.25em',
+                            fontSize: '1.5em',
                         }}
                         startDelay={2000}
                         cursorColor="#3F3D56"
                         multiText={[
-                          'Magna laboris eiusmod ex exercitation aute. Nostrud magna consectetur esse cillum sunt ea non amet reprehenderit consectetur amet dolore dolore. Elit fugiat ad velit id nisi amet incididunt occaecat do incididunt labore nulla. Sint excepteur adipisicing Lorem magna ex exercitation excepteur ex pariatur duis eu. Ipsum eiusmod adipisicing enim consectetur aliquip sunt sint nostrud. Et qui esse fugiat exercitation ex est consectetur. Velit culpa exercitation excepteur commodo cillum ea tempor velit proident commodo ipsum adipisicing eiusmod et.',
-                          'Commodo pariatur nisi culpa exercitation dolore. Non nostrud nisi ex duis officia id in mollit culpa voluptate deserunt reprehenderit nulla ipsum. Mollit Lorem non voluptate consectetur ea culpa officia dolor. Fugiat duis ex deserunt in minim laboris. Adipisicing consectetur ad aute ad eu minim reprehenderit consequat laborum laboris minim ea enim. Labore nulla ipsum magna quis elit deserunt excepteur.',
-                          'Ipsum nostrud qui magna sit veniam aliquip duis exercitation Lorem pariatur velit cillum ad anim. Occaecat consectetur duis reprehenderit qui tempor minim qui ad sint commodo nisi commodo. Duis excepteur sit minim deserunt mollit minim fugiat ea excepteur aute velit. Mollit non velit velit mollit dolore magna et labore cillum cupidatat elit esse adipisicing cillum. Ad aute quis est ut culpa.',
-                          'Voluptate consequat sit id nulla consectetur ex consectetur deserunt nisi occaecat eu et. Cillum Lorem occaecat esse laborum anim. Aliqua anim ullamco minim dolor dolor nostrud non labore ut sunt. Culpa aliquip ullamco nulla aute dolor nostrud exercitation irure do.',
-                          'Fugiat cillum cupidatat non velit ex. Deserunt eiusmod ut laborum do ipsum cillum id veniam non ut ipsum ipsum. Ullamco commodo nisi laboris velit in cillum. Proident aliquip id pariatur ut. Officia officia eu do aute velit dolore tempor magna elit anim aute laboris cillum id.',
+                          'Going bananas!',
                         ]}
                         multiTextDelay={1000}
                         typeSpeed={30}
--- src/components/SponsoriHero.jsx
+++ src/components/SponsoriHero.jsx
@@ -3,20 +3,19 @@ import joyson from '../assets/sponsors/joyson.png'
 import merpano from '../assets/sponsors/merpano.png'
 import panduit from '../assets/sponsors/panduit.png'
 
+
 function SponsoriHero(){
     return(
-    <div className="h-screen w-screen flex flex-col items-center justify-start p-[5vh]">
-            <div className="flex flex-col lg:flex-row w-screen p-[10vh]">
-                <img src={gbe} alt="" className="lg:w-[20vw] w-[30vh] lg:mr-[10vw] lg:ml-[10vw] lg:mb-0 mb-20 hover:scale-105 hover:drop-shadow-[0px_0px_35px_rgba(0,0,0,0.25)] transition-all" />
-                <img src={joyson} alt="" className="lg:w-[20vw] w-[30vh] lg:mr-[10vw] lg:ml-[10vw] lg:mb-0 mb-20 hover:scale-105 hover:drop-shadow-[0px_0px_35px_rgba(0,0,0,0.25)] transition-all" />
+    <div className="h-screen w-screen flex flex-col items-center  p-[5vh]">
+            <div className="flex flex-col lg:flex-row w-screen items-center lg:p-[10vh] pb-[10vh]">
+                <a target="_blank" href="https://www.greenbrier-europe.com/"><img src={gbe} alt="greenbrier europe" className="lg:w-[20vw] w-[30vh] lg:mr-[10vw] lg:ml-[10vw] lg:mb-0 mb-20 hover:scale-105 hover:drop-shadow-[0px_0px_35px_rgba(0,0,0,0.25)] transition-all" /></a>
+                <a target="_blank" href="https://www.joysonsafety.com/"><img src={joyson} alt="joyson" className="lg:w-[20vw] w-[30vh] lg:mr-[10vw] lg:ml-[10vw] lg:mb-0 mb-20 hover:scale-105 hover:drop-shadow-[0px_0px_35px_rgba(0,0,0,0.25)] transition-all" /></a>
             </div>
 
-            <div className="flex flex-col lg:flex-row w-screen p-[10vh]">
-            <img src={merpano} alt="" className="lg:w-[30vw] w-[30vh] lg:mr-[10vw] lg:ml-[10vw] lg:mb-0 mb-20 hover:scale-105 hover:drop-shadow-[0px_0px_35px_rgba(0,0,0,0.25)] transition-all" />
-            <img src={panduit} alt="" className="lg:w-[30vw] w-[30vh] llg:mr-[10vw] lg:ml-[10vw] lg:mb-0 mb-20 hover:scale-105 hover:drop-shadow-[0px_0px_35px_rgba(0,0,0,0.25)] transition-all" />
+            <div className="flex flex-col lg:flex-row w-screen items-center lg:p-[10vh]">
+            <a target="_blank" href="https://merpano.ro/"><img src={merpano} alt="merpano" className="lg:w-[30vw] w-[30vh] lg:mr-[10vw] lg:ml-[10vw] lg:mb-0 mb-20 hover:scale-105 hover:drop-shadow-[0px_0px_35px_rgba(0,0,0,0.25)] transition-all" /></a>
+            <a target="_blank" href="https://www.panduit.com/"><img src={panduit} alt="panduit" className="lg:w-[30vw] w-[30vh] llg:mr-[10vw] lg:ml-[10vw] lg:mb-0 mb-20 hover:scale-105 hover:drop-shadow-[0px_0px_35px_rgba(0,0,0,0.25)] transition-all" /></a>
             </div>
-
-           
     </div>
     )
 }
