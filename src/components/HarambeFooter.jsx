e1d28bd0591b20ec8aaa31ad4df344edf2b16e51
Jarvx <jarvx404@gmail.com>
2023-04-06 15:27:19 +0300

    spelling

--- src/components/HarambeFooter.jsx
+++ src/components/HarambeFooter.jsx
@@ -5,7 +5,7 @@ function HarambeFooter(){
   return(
     <footer className="footer footer-center p-1 bg-base-300 text-base-content footer-position fixed bottom-0 opacity-60 invisible lg:visible">
       <div>
-        <p>Copyright © 2023 - All right reserved by <a className="font-bold" target="_blank" href="https://github.com/HarambeCartelRobotics">Harambe Cartel Robotics</a> - made wit ❤️ by <a href="https://github.com/Jarvx404" className="font-bold">Jarvx</a> </p>
+        <p>Copyright © 2023 - All right reserved by <a className="font-bold" target="_blank" href="https://github.com/HarambeCartelRobotics">Harambe Cartel Robotics</a> - made with ❤️ by <a href="https://github.com/Jarvx404" className="font-bold">Jarvx</a> </p>
       </div>
     </footer>
   )
